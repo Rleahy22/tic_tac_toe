9fae07a846fb4032fa657a8f91ba41d8e14e988b
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-12 16:40:35 -0500

    Update db schema

--- db/migrate/20130712142234_create_games.rb
+++ db/migrate/20130712142234_create_games.rb
@@ -3,6 +3,8 @@ class CreateGames < ActiveRecord::Migration
     create_table :games do |t|
       t.belongs_to :user
       t.boolean :complete, default: false
+      t.string :board, default: "---------"
+      
       t.timestamps
     end
   end
