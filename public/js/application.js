fbc2a235ea2b8269e6ff5d59b2c4c394eaa12917
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-12 15:45:30 -0500

    started work on game play board

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
+  var player1 = "<img src=\"/images/O.jpg\">";
+  var player2 = "<img src=\"/images/X.png\">";
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('li').click(function(){
+    $(this).html(player2);
+  });
 });
