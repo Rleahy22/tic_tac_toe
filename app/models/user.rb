9675634a179ceefc685c3457ffc1866b4fe750e8
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-12 15:50:03 -0500

    updated user model to include password

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,24 +11,24 @@ class User < ActiveRecord::Base
 	has_many :played_games, :through => :moves, :source => :game
 
 
-	# def password
-	# 	@password ||= Password.new(password_hash)
-	# end
+	def password
+		@password ||= Password.new(password_hash)
+	end
 
-	# def password=(new_password)
-	# 	@password = Password.create(new_password)
-	# 	self.password_hash = @password
-	# end
+	def password=(new_password)
+		@password = Password.create(new_password)
+		self.password_hash = @password
+	end
 
-	# def self.create(params)
-	# 	@user = User.new(params)
-	# 	@user.password = params[:password]
-	# 	@user.save!
-	# 	@user
-	# end
+	def self.create(params)
+		@user = User.new(params)
+		@user.password = params[:password]
+		@user.save!
+		@user
+	end
 
-	# def self.verify(params)
-	# 	@user = User.find_by_name(params[:name])
-	# 	@user.password == params[:password] ? @user : nil
-	# end
+	def self.verify(params)
+		@user = User.find_by_name(params[:name])
+		@user.password == params[:password] ? @user : nil
+	end
 end
